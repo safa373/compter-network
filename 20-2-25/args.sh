0b7039c040a2d8739123396c4efe280852a0b870
safa373 <safamariyam33@gmail.com>
2025-03-06 10:46:20 +0530

    Add files via upload

---
+++ 20-2-25/args.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Total Arguments :$#"
+echo "First :$1"
+echo "Second :$2"
+result=$(($1+$2))
+echo $result
---
+++ 20-2-25/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+var=2
+var2=3
+var3=$((var+var2))
+echo "$var + $var2 is $var3"
